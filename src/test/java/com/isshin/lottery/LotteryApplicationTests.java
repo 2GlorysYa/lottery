db6ecf41cf3dd5e3fee765be38744c507fae67ec
Yibo Lei <ryanyibo45@outlook.com>
2023-05-14 23:35:36 +0800

    init commit

---
+++ src/test/java/com/isshin/lottery/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isshin.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
